306f7ca1537dd17ceafaa7806fc4500471c68d38
ZakGhost354313 <mbm3543@gmail.com>
2022-09-03 10:18:25 -0500

    second commit

---
+++ bot.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const Discord = require("discord.js");
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const prefix = "!"
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+client.on("message", msg => {
+    switch (msg.content) {
+        case '${prefix}ping':
+            msg.reply('pong');
+    }
+})
+
+client.login(process.env.DISCORD_TOKEN);
